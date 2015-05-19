4450f79ac3c940bcf65cbae987fc472c02985442
Mike Talbott <mtalbott@harrislocalgov.com>
2015-05-18 21:11:36 -0400

    Filter plug-in assemblies for those not already in the current app domain.

--- Core/PluginLoader.fs
+++ Core/PluginLoader.fs
@@ -4,7 +4,17 @@ open System.IO
 open System.Reflection
 open SSP.Core
 
-let files path = Directory.GetFiles(path,"*.Plugin*.dll") |> Seq.toList
+let files path = 
+    let loadedAssemblies = 
+        AppDomain.CurrentDomain.GetAssemblies() 
+        |> Seq.map (fun x -> x.FullName.Split(',') |> Seq.head)
+    let foundAssemblyFiles = 
+        Directory.GetFiles(path,"*.dll") 
+    let nonLoadedAssemblyFiles =
+        foundAssemblyFiles
+        |> Seq.filter (fun x -> not (Seq.exists ((=) (Path.GetFileNameWithoutExtension(x))) loadedAssemblies)) 
+        |> Seq.toList
+    nonLoadedAssemblyFiles
 
 let getPluginsFromFile file =
         let a = Assembly.LoadFrom(file)
@@ -13,13 +23,11 @@ let getPluginsFromFile file =
                 if i.Name = "IPlugin" && i.Namespace = "SSP.Core" then
                     let pluginObject = 
                         a.CreateInstance(t.FullName)
-                        :?> IPlugin
-                    yield pluginObject ]
-//                    match pluginObject with 
-//                    | :? IPlugin -> 
-//                        printfn "Valid plugin loaded %s" t.FullName
-//                        yield a.CreateInstance(t.FullName) :?> IPlugin
-//                    | _ -> printfn "Invalid plugin %s" t.FullName ]
+                    match pluginObject with 
+                    | :? IPlugin -> 
+                        printfn "Valid plugin loaded %s" t.FullName
+                        yield a.CreateInstance(t.FullName) :?> IPlugin
+                    | _ -> printfn "Invalid plugin %s" t.FullName ]
 
 let loadFromPath path =
     let f = files path
